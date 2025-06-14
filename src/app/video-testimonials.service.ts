7274b3504b6f28f1a24d21775a440b16480cba53
Jason-Holliday <105673390+Jason-Holliday@users.noreply.github.com>
2025-06-14 21:33:23 +0200

    add video thumbnail

--- src/app/video-testimonials.service.ts
+++ src/app/video-testimonials.service.ts
@@ -10,10 +10,10 @@ export class VideoTestimonialsService {
 
   getVideoTestimonials() {
     return [
-      { video: 'IMG_0608.MP4', thumbnail: '/IMG_0608.MP4' },
-      { video: 'IMG_0609.MP4', thumbnail: '/IMG_0609.MP4' },
-      { video: 'IMG_0611.MP4', thumbnail: '' },
-      { video: 'IMG_0612.MP4', thumbnail: '' }
+      { video: 'IMG_0608.MP4', thumbnail: '/thumbnail_0608.png' },
+      { video: 'IMG_0609.MP4', thumbnail: '/thumbnail_0609.png' },
+      { video: 'IMG_0611.MP4', thumbnail: '/thumbnail_0611.png' },
+      { video: 'IMG_0612.MP4', thumbnail: '/thumbnail_0612.png' }
     ];
   }
 
