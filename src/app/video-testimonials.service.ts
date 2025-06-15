3e7914fe866db5be8e1eb6006de52d5371578623
Jason-Holliday <105673390+Jason-Holliday@users.noreply.github.com>
2025-06-15 10:43:07 +0200

    Fix

--- src/app/video-testimonials.service.ts
+++ src/app/video-testimonials.service.ts
@@ -3,21 +3,27 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-
-
 export class VideoTestimonialsService {
+  constructor() {}
 
-  constructor() { }
-
-
-  getVideoTestimonials(){
+  getVideoTestimonials() {
     return [
-      { video: 'IMG_0608.MP4'},
-      { video: 'IMG_0609.MP4'},
-      { video: 'IMG_0611.MP4' },
-      { video: 'IMG_0612.MP4'}
+      {
+        video: 'IMG_0608.MP4',
+        poster: 'IMG_0608.jpg'
+      },
+      {
+        video: 'IMG_0609.MP4',
+        poster: 'IMG_0609.jpg'
+      },
+      {
+        video: 'IMG_0611.MP4',
+        poster: 'IMG_0611.jpg'
+      },
+      {
+        video: 'IMG_0612.MP4',
+        poster: 'IMG_0612.jpg'
+      }
     ];
   }
-
-
 }
